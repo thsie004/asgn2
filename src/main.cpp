bcd29ddf16e8fedec8d59b50a400515ef918e7fc
Tom Hsieh <thsie004@hammer.cs.ucr.edu>
2016-02-11 01:05:44 -0800

    added exit command

--- src/main.cpp
+++ src/main.cpp
@@ -6,5 +6,27 @@
 using namespace std;
 
 int main() {
+    //Gets user name and host name
+    char hostname[HOST_NAME_MAX];
+    char username[LOGIN_NAME_MAX];
+    int result;
+
+    result = gethostname(hostname, HOST_NAME_MAX);
+    if(result){
+        perror("gethostname");
+        return 1;
+    }
+
+    result = getlogin_r(username, LOGIN_NAME_MAX);
+    if(result){
+       perror("getlogin_r");
+       return 1;
+    }
+    //end of log in info
+
+    
+
+
+
     return 0;
 }
