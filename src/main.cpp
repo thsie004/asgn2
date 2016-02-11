23eb44f91dc43d912e62d7c5c2015460a8d8d329
Wangho Ha <wha004@hammer.cs.ucr.edu>
2016-02-10 23:36:45 -0800

    deleted some test code for main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,33 +15,5 @@ int main() {
         cout << tokens[i] << " size " << tokens[i].size() << endl;
     }
 
-/*Wangho: TEST AREA*/
-    string ls = "ls";
-    string dash = "-l";
-    char* cmd[] = {(char*)ls.c_str(), (char*)dash.c_str(), NULL};
-
-    string ls2 = "ls";
-    string dash2 = "-a";
-    char* cmd2[] = {(char*)ls2.c_str(), (char*)dash2.c_str(), NULL};
-
-    string connect = ";";       
-
-    if (connect == ";" /*Wangho: if connector is ";"*/) {
-        execute(cmd);
-        execute(cmd2);
-    }else if (connect == "&&" /*Wangho: if connecter is "&&"*/) {
-        output = execute(cmd);
-
-        if (output == 0) {
-            execute(cmd2);
-        }
-    }else if (connect == "||" /*Wangho: if connecter is "||"*/) {
-        output = execute(cmd);
-
-        if (output == -1) {
-            execute(cmd2);
-        }
-    }
-/*end TEST AREA*/
     return 0;
 }
