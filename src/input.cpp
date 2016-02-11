a46c4bc7fb60bb89fb20e6901502355fcf1b4738
Tom Hsieh <thsie004@ucr.edu>
2016-02-10 19:08:34 -0800

    organized input.cpp

--- src/input.cpp
+++ src/input.cpp
@@ -146,50 +146,8 @@ void getInput(vector<string> &tokenz) {
 }
 
 //IMPORTANT!: TOM: I have moved your execute to words.h and words.cpp because
-//this function will only be used by that class, and not in main. Please update
+//this function will only be used by that class, and not for inputs. Please update
 //this function in those files when you start working.
-
-int main() {
-    vector<string> tokens;
-    getInput(tokens);
-    int output = 0;
-
-    cout << "tokens content: " << endl;
-    for (unsigned int i = 0; i < tokens.size(); i++) {
-        cout << tokens[i] << " size " << tokens[i].size() << endl;
-    }
-
-/*Wangho: TEST AREA*/
-    string ls = "ls";
-    string dash = "-l";
-    char* cmd[] = {(char*)ls.c_str(), (char*)dash.c_str(), NULL};
-
-    string ls2 = "ls";
-    string dash2 = "-a";
-    char* cmd2[] = {(char*)ls2.c_str(), (char*)dash2.c_str(), NULL};
-
-    string connect = ";";       
-
-    if (connect == ";" /*Wangho: if connector is ";"*/) {
-        execute(cmd);
-        execute(cmd2);
-    }else if (connect == "&&" /*Wangho: if connecter is "&&"*/) {
-        output = execute(cmd);
-
-        if (output == 0) {
-            execute(cmd2);
-        }
-    }else if (connect == "||" /*Wangho: if connecter is "||"*/) {
-        output = execute(cmd);
-
-        if (output == -1) {
-            execute(cmd2);
-        }
-    }
-/*end TEST AREA*/
-    return 0;
-}
-
 
 #endif
 
