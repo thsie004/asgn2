b28342c731722d15ed6a3f4b6fb93366ef4e9e2d
Tom Hsieh <thsie004@hammer.cs.ucr.edu>
2016-02-12 13:36:31 -0800

    edited test scripts

---
+++ tests/multi_command.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#DO NOT REMOVE
+echo The test script assumes you are in rshell and will run rshell with
+echo the command "./bin/rshell" while feeding it input with heredoc.
+#DO NOT REMOVE
+
+
+
