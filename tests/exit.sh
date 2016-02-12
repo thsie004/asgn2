65ab9cc2f09c80164fe48bd193a2af73e39bc577
Wangho Ha <wha004@hammer.cs.ucr.edu>
2016-02-12 14:15:18 -0800

    just started bash script

--- tests/exit.sh
+++ tests/exit.sh
@@ -5,5 +5,13 @@ echo The test script assumes you are in rshell and will run rshell with
 echo the command "./bin/rshell" while feeding it input with heredoc.
 #DO NOT REMOVE
 
+~/rshell/bin/rshell << GUARD
+echo hello world
+ls -l #helloooooo
+ping -c 5 www.google.com
+git status
+
+
+GUARD
 
 
--- tests/single_command.sh
+++ tests/single_command.sh
@@ -5,4 +5,23 @@ echo The test script assumes you are in rshell and will run rshell with
 echo the command "./bin/rshell" while feeding it input with heredoc.
 #DO NOT REMOVE
 
+echo Start testing single_command
+echo ============================
+
+~/rshell/bin/rshell << GUARD
+echo hello world
+ls -l
+ping -c 3 www.google.com
+echo start error test
+cdd
+lss
+pwwd
+echo back to good
+pwd
+git status
+lss -a
+ls -a
+
+GUARD
+
 
